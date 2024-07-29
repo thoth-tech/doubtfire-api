90cc8f32275c6e919c43c7f39370ee5fe188fe3f
Jake - Bakanai <51482783+DudeUnleashed@users.noreply.github.com>
2024-07-29 17:34:58 +1000

    fix: Update schema.rb
    removed the git diff comments left over from rebasing to 9.x from development

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2024_05_28_223908) do
-=======
-ActiveRecord::Schema[7.1].define(version: 2024_05_21_080243) do
->>>>>>> 8ca4214e (fix: add courseapi)
   create_table "activity_types", charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "abbreviation", null: false
