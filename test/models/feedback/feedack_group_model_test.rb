f7b782de2e9dacefb9e4a6dd2da7a509f7ca1186
Coskun Kilinc <82747562+coskun-kilinc@users.noreply.github.com>
2024-08-05 13:04:38 +1000

    feat: add feedback group model tests
    The code changes include the addition of tests for
    the FeedbackGroup model and updated tests for the
    FeedbackCommentTemplate model. These tests ensure
    that a valid feedback group can be created and
    that invalid feedback groups are handled
    correctly.

---
+++ test/models/feedback/feedack_group_model_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class FeedbackGroupTest < ActiveSupport::TestCase
+
+  # Set up variables for testing
+  setup do
+    @title = Faker::Lorem.sentence
+    @feedback_comment_template = FactoryBot.create(:feedback_comment_template)
+    @task_definition = FactoryBot.create(:task_definition)
+  end
+
+  # Test that you can create a valid feedback group
+  def test_valid_feedback_group_creation
+    feedback_group = FeedbackGroup.create!(title: @title, feedback_comment_template: @feedback_comment_template, task_definition: @task_definition)
+
+    assert feedback_group.valid? # "assert": pass if true, i.e. pass if feedback_group exists
+    assert_equal @title, feedback_group.title
+    assert_equal @feedback_comment_template, feedback_group.feedback_comment_template
+    assert_equal @task_definition, feedback_group.task_definition
+  end
+
+  # Test that you cannot create an invalid feedback group
+  def test_invalid_feedback_group_creation
+
+      # Test that feedback group is invalid without title
+      feedback_group = FeedbackGroup.new(feedback_comment_template: @feedback_comment_template, task_definition: @task_definition)
+      refute feedback_group.valid? # pass if feedback
+
+      # Test that feedback group is invalid without feedback comment template
+      feedback_group = FeedbackGroup.new(title: @title, task_definition: @task_definition)
+      refute feedback_group.valid?
+
+      # Test that feedback group is invalid without task definition
+      feedback_group = FeedbackGroup.new(title: @title, feedback_comment_template: @feedback_comment_template)
+      refute feedback_group.valid?
+
+      # Test that feedback group is valid with title, feedback comment template
+      # and task definition
+      feedback_group.title = @title
+      feedback_group.feedback_comment_template = @feedback_comment_template
+      feedback_group.task_definition = @task_definition
+      assert feedback_group.valid?
+
+      # Test that feedback group is invalid with title and without feedback
+      # comment template
+      feedback_group.feedback_comment_template = nil
+      refute feedback_group.valid?
+      assert_includes feedback_group.errors[:feedback_comment_template], "can't be blank"
+
+      # Test that feedback group is invalid with feedback comment template and
+      # without title
+      feedback_group.title = nil
+      feedback_group.feedback_comment_template = @feedback_comment_template
+      refute feedback_group.valid?
+      assert_includes feedback_group.errors[:title], "can't be blank"
+
+      # Test that feedback group is invalid with title and without task
+      # definition
+      feedback_group.title = @title
+      feedback_group.task_definition = nil
+      refute feedback_group.valid?
+      assert_includes feedback_group.errors[:task_definition], "can't be blank"
+
+      # Test that feedback group is unsaved
+      refute feedback_group.save # fail if feedback_group is saved
+  end
--- test/models/feedback/feedback_comment_template_model_test.rb
+++ test/models/feedback/feedback_comment_template_model_test.rb
@@ -2,39 +2,81 @@ require 'test_helper'
 
 #
 # Contains tests for FeedbackCommentTemplate model objects - not accessed via API
-# 
+#
 class FeedbackCommentTemplateModelTest < ActiveSupport::TestCase
+  # class FeedbackCommentTemplate {
+	# 	-string: Abbreviation
+	# 	-number: Order
+	# 	-char[20]: ChipText
+	# 	-string: Description
+	# 	-string: CommentText
+	# 	-string: SummaryText
+	# 	-TaskStatus: TaskStatus
 
-    # Setup objects for testing
-    def setup
-        task_status = TaskStatus.new
-        @criterion_option = CriterionOption.create(task_status: task_status)
-    end
+	# }
 
-    # Test you can create a valid feedback comment template
-    def test_valid_feedback_comment_template_creation
-        feedback_comment_template = FeedbackCommentTemplate.create(criterion_option: @criterion_option, comment_text_situation: 'This is the situation')
+  # Set up variables for testing
+  setup do
+    @abbreviation = Faker::Lorem.word
+    @order = Faker::Number.number(digits: 1)
+    @chip_text = Faker::Lorem.characters(number: 20)
+    @description = Faker::Lorem.sentence
+    @comment_text = Faker::Lorem.sentence
+    @summary_text = Faker::Lorem.sentence
+    @task_status = FactoryBot.create(:task_status)
+  end
 
-        assert feedback_comment_template.valid?
-        
-        assert feedback_comment_template.criterion_option == @criterion_option
-        assert feedback_comment_template.comment_text_situation == 'This is the situation'
-    end
+  # Test that you can create a valid feedback comment template
+  def test_valid_feedback_comment_template_creation
+    feedback_comment_template = FeedbackCommentTemplate.create!(abbreviation: @abbreviation, order: @order, chip_text: @chip_text, description: @description, comment_text: @comment_text, summary_text: @summary_text, task_status: @task_status)
 
-    # Test you cannot create an invalid feedback comment template
-    def test_invalid_feedback_comment_template_creation
-        feedback_comment_template = FeedbackCommentTemplate.new
-        refute feedback_comment_template.valid?
+    assert feedback_comment_template.valid? # "assert": pass if true, i.e. pass if feedback_comment_template exists
+    assert_equal @abbreviation, feedback_comment_template.abbreviation
+    assert_equal @order, feedback_comment_template.order
+    assert_equal @chip_text, feedback_comment_template.chip_text
+    assert_equal @description, feedback_comment_template.description
+    assert_equal @comment_text, feedback_comment_template.comment_text
+    assert_equal @summary_text, feedback_comment_template.summary_text
+    assert_equal @task_status, feedback_comment_template.task_status
+  end
 
-        feedback_comment_template.comment_text_situation = 'This is a comment'
-        refute feedback_comment_template.valid?
+  # Test that you cannot create an invalid feedback comment template
+  def test_invalid_feedback_comment_template_creation
 
-        feedback_comment_template.criterion_option = @criterion_option
-        assert feedback_comment_template.valid?
+      # Test that feedback comment template is invalid without abbreviation
+      feedback_comment_template = FeedbackCommentTemplate.new(order: @order, chip_text: @chip_text, description: @description, comment_text: @comment_text, summary_text: @summary_text, task_status: @task_status)
+      refute feedback_comment_template.valid? # pass if feedback
 
-        feedback_comment_template.comment_text_situation = nil
-        refute feedback_comment_template.valid?
+      # Test that feedback comment template is invalid without order
+      feedback_comment_template = FeedbackCommentTemplate.new(abbreviation: @abbreviation, chip_text: @chip_text, description: @description, comment_text: @comment_text, summary_text: @summary_text, task_status: @task_status)
+      refute feedback_comment_template.valid?
 
-        refute feedback_comment_template.save
-    end
-end
+      # Test that feedback comment template is invalid without chip text
+      feedback_comment_template = FeedbackCommentTemplate.new(abbreviation: @abbreviation, order: @order, description: @description, comment_text: @comment_text, summary_text: @summary_text, task_status: @task_status)
+      refute feedback_comment_template.valid?
+
+      # Test that feedback comment template is invalid without description
+      feedback_comment_template = FeedbackCommentTemplate.new(abbreviation: @abbreviation, order: @order, chip_text: @chip_text, comment_text: @comment_text, summary_text: @summary_text, task_status: @task_status)
+      refute feedback_comment_template.valid?
+
+      # Test that feedback comment template is invalid without comment text
+      feedback_comment_template = FeedbackCommentTemplate.new(abbreviation: @abbreviation, order: @order, chip_text: @chip_text, description: @description, summary_text: @summary_text, task_status: @task_status)
+      refute feedback_comment_template.valid?
+
+      # Test that feedback comment template is invalid without summary text
+      feedback_comment_template = FeedbackCommentTemplate.new(abbreviation: @abbreviation, order: @order, chip_text: @chip_text, description: @description, comment_text: @comment_text, task_status: @task_status)
+      refute feedback_comment_template.valid?
+
+      # Test that feedback comment template is valid with abbreviation, order, chip text, description, comment text, summary text and task status
+      feedback_comment_template.abbreviation = @abbreviation
+      feedback_comment_template.order = @order
+      feedback_comment_template.chip_text = @chip_text
+      feedback_comment_template.description = @description
+      feedback_comment_template.comment_text = @comment_text
+      feedback_comment_template.summary_text = @summary_text
+      feedback_comment_template.task_status = @task_status
+      assert feedback_comment_template.valid?
+
+      # Test that the feedback comment template is unsaved
+      refute feedback_comment_template.save # fail if feedback_comment_template is saved
+  end
