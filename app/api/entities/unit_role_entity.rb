a35aea12f4c23408e7c84a298fc66dd8f0d02dee
Atharv Bhandare <atharvbhandare2001@gmail.com>
2025-05-06 11:30:36 +1000

    Add role-based checks in UnitRoleEntity to prevent students from accessing full staff details

--- app/api/entities/unit_role_entity.rb
+++ app/api/entities/unit_role_entity.rb
@@ -2,7 +2,15 @@ module Entities
   class UnitRoleEntity < Grape::Entity
     expose :id
     expose :role do |unit_role, options| unit_role.role.name end
-    expose :user, using: Entities::Minimal::MinimalUserEntity
+
+    # Replaced static MinimalUserEntity with conditional branching to prevent students from seeing full staff details (violates least privilege)
+    expose :user, using: Entities::UserEntity, if: lambda { |_unit_role, options|
+      Entities::UnitEntity.is_staff?(options[:my_role])
+    }
+    # Used Entities::UnitEntity.is_staff? for permission check to use centralized and reusable RBAC logic
+    expose :user, using: Entities::Minimal::MinimalUserEntity, unless: lambda { |_unit_role, options|
+      Entities::UnitEntity.is_staff?(options[:my_role])
+    }
     expose :unit, using: Entities::Minimal::MinimalUnitEntity, unless: :in_unit
   end
 end
