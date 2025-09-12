ea33184443c91e5a31f6b0e5aaf471623f91de5f
JosephKS10 <jjbrothersdelhi@hotmail.com>
2025-09-13 00:32:25 +1000

    Made changes to the code for Task 1

--- db/migrate/20250912012227_add_tutorial_self_enrolment_to_task_definitions.rb
+++ db/migrate/20250912012227_add_tutorial_self_enrolment_to_task_definitions.rb
@@ -1,17 +1,13 @@
 class AddTutorialSelfEnrolmentToTaskDefinitions < ActiveRecord::Migration[7.1]
   def change
-    add_column :task_definitions, :tutorial_self_enrolment_enabled, :boolean, default: false, null: false
-    add_reference :task_definitions, :tutorial_self_enrolment_stream, foreign_key: { to_table: :tutorial_streams }, null: true
+    unless column_exists?(:task_definitions, :tutorial_self_enrolment_enabled)
+      add_column :task_definitions, :tutorial_self_enrolment_enabled, :boolean, default: false, null: false
+      add_index :task_definitions, :tutorial_self_enrolment_enabled
+    end
 
-    # Add index for better query performance
-    add_index :task_definitions, :tutorial_self_enrolment_enabled
-    add_index :task_definitions, :tutorial_self_enrolment_stream_id
-  end
-
-  def down
-    remove_index :task_definitions, :tutorial_self_enrolment_enabled
-    remove_index :task_definitions, :tutorial_self_enrolment_stream_id
-    remove_reference :task_definitions, :tutorial_self_enrolment_stream
-    remove_column :task_definitions, :tutorial_self_enrolment_enabled
+    unless column_exists?(:task_definitions, :tutorial_self_enrolment_stream_id)
+      add_reference :task_definitions, :tutorial_self_enrolment_stream, foreign_key: { to_table: :tutorial_streams }, null: true
+      add_index :task_definitions, :tutorial_self_enrolment_stream_id
+    end
   end
 end
