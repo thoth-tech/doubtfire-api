05b690d92fd2e77a49d33a3d86cb74a4ae93c6c1
Steven Dalamaras <sd@stevendalamaras.dev>
2026-04-26 21:02:07 +1000

    feat(ai_eff): default value for predicted effort
    Also, the addition of the default value to any record
    that has null values in the column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_25_165056) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_26_101725) do
   create_table "activity_types", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "abbreviation", null: false
@@ -450,7 +450,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_04_25_165056) do
     t.boolean "use_resources_for_jplag_base_code", default: false, null: false
     t.boolean "lock_assessments_to_tutorial_stream", default: false, null: false
     t.boolean "requires_discussion", default: false, null: false
-    t.float "predicted_effort"
+    t.float "predicted_effort", default: 1.0
     t.index ["abbreviation", "unit_id"], name: "index_task_definitions_on_abbreviation_and_unit_id", unique: true
     t.index ["group_set_id"], name: "index_task_definitions_on_group_set_id"
     t.index ["name", "unit_id"], name: "index_task_definitions_on_name_and_unit_id", unique: true
